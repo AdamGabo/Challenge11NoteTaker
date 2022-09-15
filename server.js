6bbe1b645552e347fb39bd96c90a592ee8444116
Adam Gaboury <adam.gaboury@hotmail.com>
2022-09-15 01:13:25 -0400

    server.js update, and main functions implementation

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
+//ADD ALL NECESSARY ELEMENTS FOR THE BACKEND APP
+
+//USE 3001 AS PER MODULE FOR ROUTING, ADD/DECLARE ELEMENTS AS PER MODULE 
 const PORT = process.env.PORT || 3001;
-
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+const noteItems = require('./db/db.json');
 
-const { animals } = require('./data/animals');
+//INTIALIZE GET METHODS AS PER ASSIGNMENT REQUIREMENTS, UTILIZE MODULE METHOD EXAMPLE 
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, './public/index.html'));});
+app.get('*', (req, res) => {res.sendFile(path.join(__dirname, './public/index.html'));});
+app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, './public/notes.html'));});
+app.get('/api/notes', (req, res) => {res.json(noteItems);});
+//app.get('/api/notes', (req, res) => {res.json(noteItems.slice(1));});
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
-  
-  app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-  });
+//ADD ELEMENT FUNCTION
+function addElement(note, array) {
+    
+    note.id = array[0];
+    array.push(note);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(array, null, 2));
+    return note;
+}
+//POST ELEMENT UTILIZING THE addElement FUNCTION
+app.post('/api/notes', (req, res) => {res.json(addElement(req.body, noteItems));});
+//BONUS DELETE 
+
+//DELETE ELEMENT BASED ON ID 
+function deleteElement(id, array) {
+    //FOR LOOP WHICH GOES THROUGH EACH ITEM IN THE ARRAY 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id == id) {
+            //GET ONLY THAT ONE ELEMENT IN THE ARRAY USING THE SPLICE METHOD
+            array.splice(i, 1);
+            //FS WRITE FILE 
+            fs.writeFileSync(path.join(__dirname, './db/db.json'),JSON.stringify(array, null, 2));
+            //BREAK ONCE THE DESIRED ELEMENT HAS BEEN DELETED 
+            break;
+        }
+    }
+}
+//DELETE ELEMENT BASED OFF OF ID 
+app.delete('/api/notes/:id', (req, res) => {deleteElement(req.params.id, noteItems);res.json(true);});
+//LISTEN METHOD FOR SERVER Express.js, AS PER MODULE 
+app.listen(PORT, () => {console.log(`API server now on port ${PORT}!`);});
