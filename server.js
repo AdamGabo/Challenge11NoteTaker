2c58c727403472bfb6d5346405679275e499d936
Adam Gaboury <adam.gaboury@hotmail.com>
2022-09-15 17:10:33 -0400

    changed db.json format, modified addElement function and deleteElement function, added validation process, debugging

--- server.js
+++ server.js
@@ -6,38 +6,46 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const noteItems = require('./db/db.json');
+const { notes } = require('./db/db.json');
+//const noteItems = require('./db/db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
 //INTIALIZE GET METHODS AS PER ASSIGNMENT REQUIREMENTS, UTILIZE MODULE METHOD EXAMPLE 
-app.get('/api/notes', (req, res) => {res.json(noteItems);});
+app.get('/api/notes', (req, res) => {res.json(notes);});
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, './public/index.html'));});
 app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, './public/notes.html'));});
 app.get('*', (req, res) => {res.sendFile(path.join(__dirname, './public/index.html'));});
 
-//app.get('/api/notes', (req, res) => {res.json(noteItems.slice(1));});
-
-//ADD ELEMENT FUNCTION
-// function addElement(note, body) {
-
-
-//     note.id = body[0];
-//     body.push(note);
-//     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(body, null, 2));
-//     return note;
-// }
 function addElement(notes, body) {
     const note = body;
+    notes.id = body[0]; 
     notes.push(note);
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({notes}, null, 2));
     return note;
 }
 //POST ELEMENT UTILIZING THE addElement FUNCTION
-app.post('/api/notes', (req, res) => {res.json(addElement(req.body, noteItems));});
+function validateNotes(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+        return true;
+}
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+    if (!validateNotes(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = addElement(notes,req.body);
+        res.json(note);
+    }
+});
 //BONUS DELETE 
 
 //DELETE ELEMENT BASED ON ID 
@@ -55,6 +63,6 @@ function deleteElement(id, body) {
     }
 }
 //DELETE ELEMENT BASED OFF OF ID 
-app.delete('/api/notes/:id', (req, res) => {deleteElement(req.params.id, noteItems);res.json(true);});
+app.delete('/api/notes/:id', (req, res) => {deleteElement(req.params.id, notes);res.json(true);});
 //LISTEN METHOD FOR SERVER Express.js, AS PER MODULE 
 app.listen(PORT, () => {console.log(`port: ${PORT}!`);});
